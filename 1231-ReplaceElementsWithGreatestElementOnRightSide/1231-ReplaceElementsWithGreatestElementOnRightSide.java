e236af138eaeac5bab24ed521685679bcc121add
joelanand2006 <144458772+joelanand2006@users.noreply.github.com>
2026-02-04 20:20:33 +0530

    Create 1231-ReplaceElementsWithGreatestElementOnRightSide

---
+++ 1231-ReplaceElementsWithGreatestElementOnRightSide/1231-ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,14 @@
+// Last updated: 2/4/2026, 8:20:31 PM
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            int max=-1;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]>max) max=arr[j];
+
+            }
+            arr[i]=max;
+        }
+        return arr;
+    }
+}
