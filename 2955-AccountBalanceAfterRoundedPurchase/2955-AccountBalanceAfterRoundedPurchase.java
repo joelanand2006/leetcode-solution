521e110713085ba89fea1678585e2d2400d3fe3f
joelanand2006 <144458772+joelanand2006@users.noreply.github.com>
2026-02-04 20:20:06 +0530

    Create 2955-AccountBalanceAfterRoundedPurchase

---
+++ 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,7 @@
+// Last updated: 2/4/2026, 8:20:04 PM
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        if(purchaseAmount%10<5) return 100-((purchaseAmount/10)*10);
+        else return 100-((purchaseAmount/10)*10)-10;
+    }
+}
